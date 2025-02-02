f5ed1aa6a080023fc3424c5de8c3e2dc0ef272ef
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-02-02 14:30:33 +0900

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    // "destructuring assignment"
+    if (start > end) {
+        start = start + end;
+        end = start - end;
+        start = start -end;
+    };
 
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    
+    let sum = 0; 
+    for (i = start; i <= end; i++) sum += i;
+    return sum
 };
 
 // Do not edit below this line
